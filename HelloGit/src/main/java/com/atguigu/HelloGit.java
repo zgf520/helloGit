9d551dfa89dbcbaab89ad2cf4b8773d4a0f5dc34
zgf520 <1520813222@qq.com>
2021-08-19 21:45:59 +0800

    测试SSH

--- HelloGit/src/main/java/com/atguigu/HelloGit.java
+++ HelloGit/src/main/java/com/atguigu/HelloGit.java
@@ -5,5 +5,6 @@ public class HelloGit {
         System.out.println("Hello Git~");
         System.out.println("创建分支");
         System.out.println("测试SSH");
+        System.out.println("测试阿斯顿啊阿斯顿教师节覅哦昂说");
     }
 }
