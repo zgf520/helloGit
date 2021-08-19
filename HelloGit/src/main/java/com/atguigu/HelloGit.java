28241bbd747aaa47ea86070dfdfab02f1c4c232f
zgf520 <1520813222@qq.com>
2021-08-19 20:38:27 +0800

    创建分支

--- HelloGit/src/main/java/com/atguigu/HelloGit.java
+++ HelloGit/src/main/java/com/atguigu/HelloGit.java
@@ -3,8 +3,6 @@ package com.atguigu;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git~");
-        System.out.println("更新1");
-        System.out.println("更新2");
-        System.out.println("更新3");
+        System.out.println("创建分支");
     }
 }
